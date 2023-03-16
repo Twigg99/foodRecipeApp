ad6b24ab00d7404f40abf2b26b5a213f251b7817
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2023-03-16 12:25:44 +0000

    new activities and menus have been created. Also error for duplicating files keep coming up...

--- app/src/main/java/com/example/foodrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.foodrecipeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.foodrecipeapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadFragment(HomeFragment())
+
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.profile, menu)
+            return true
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            val itemId = item.itemId
+
+            when(itemId) {
+                R.id.profile -> {
+                    //launch the settings activity when user clicks itemID
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+            return super.onOptionsItemSelected(item)
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.HomeIcon -> loadFragment(HomeFragment())
+                R.id.SearchIcon -> loadFragment(SearchFragment())
+                R.id.SaveIcon -> loadFragment(SaveFragment())
+                else -> {
+
+                }
+            }
+            true
+
+        }
+
     }
+    private  fun loadFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager.beginTransaction()
+        fragmentManager.replace(R.id.frameLayout,fragment)
+        fragmentManager.commit()
+    }
+
 }
