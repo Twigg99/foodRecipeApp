d051446cabcf0fd529dd0882a5fcf1d1b65acb57
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2023-03-17 14:31:03 +0000

    splash screen has been updated

--- app/src/main/java/com/example/foodrecipeapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.foodrecipeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.foodrecipeapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.forgotPassword.setOnClickListener{
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.RegisterHere.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
