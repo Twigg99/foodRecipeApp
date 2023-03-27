737254748213df1dabded02cdee06dfcf0a2dd16
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2023-03-27 17:49:27 +0100

    changes to the xml of the login and register activity, as well as functionality

--- app/src/main/java/com/example/foodrecipeapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/LoginActivity.kt
@@ -3,25 +3,66 @@ package com.example.foodrecipeapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import com.example.foodrecipeapp.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.RegisterHere.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
         binding.forgotPassword.setOnClickListener{
             val intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
         }
 
-        binding.RegisterHere.setOnClickListener {
-            val intent = Intent(this, ForgotPasswordActivity::class.java)
-            startActivity(intent)
+        binding.button.setOnClickListener {
+            val email = binding.emailAddress.text.toString()
+            val password = binding.password.text.toString()
+            if(email.isEmpty() || password.isEmpty()) {
+                Snackbar.make(binding.root, "Please fill in all fields", Snackbar.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            // Validate email format
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+               // binding.emailAddress.error = "Invalid email format"
+                Snackbar.make(binding.root, "Invalid Email", Snackbar.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        Log.d("LoginActivity" , "signInWithEmail was successful")
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Log.w("LoginActivity", "signInWithEmail failed")
+                        Toast.makeText(this,  it.exception?.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please fill information", Toast.LENGTH_LONG).show()
+            }
         }
+
     }
 }
+
--- app/src/main/java/com/example/foodrecipeapp/RegisterActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/RegisterActivity.kt
@@ -1,11 +1,87 @@
 package com.example.foodrecipeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.foodrecipeapp.databinding.ActivityRegisterBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        setupViews()
     }
-}
+
+    private fun setupViews() {
+        binding.signInHereText.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        val today = Calendar.getInstance()
+        val year = today.get(Calendar.YEAR)
+        val month = today.get(Calendar.MONTH) + 1
+        val day = today.get(Calendar.DAY_OF_MONTH)
+        binding.editTextDate.setText("$day/$month/$year")
+        binding.registerButton.setOnClickListener {
+            val email = binding.emailAddress.text.toString()
+            val password = binding.password.text.toString()
+            val name = binding.personName.text.toString()
+            val phoneNum = binding.TextPhone.text.toString()
+            val dateOfBirth = binding.editTextDate.text.toString()
+
+            val userMap = hashMapOf(
+                "Name" to name,
+                "Email" to email,
+                "password" to password,
+                "phone" to phoneNum,
+                "DOB" to dateOfBirth
+            )
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val userID = firebaseAuth.currentUser!!.uid
+                        db.collection("Users").document(userID).set(userMap)
+                            .addOnSuccessListener {
+
+                                Snackbar.make(binding.root, "User added", Snackbar.LENGTH_LONG)
+                                    .show()
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                            }
+                            .addOnFailureListener { exception ->
+
+                                Snackbar.make(
+                                    binding.root,
+                                    "Failed to add user",
+                                    Snackbar.LENGTH_LONG
+                                ).show()
+                                Log.e(
+                                    "RegisterActivity",
+                                    "Failed to add data to Firestore",
+                                    exception
+                                )
+                            }
+                    } else {
+
+                        Toast.makeText(this, task.exception.toString(), Toast.LENGTH_LONG).show()
+                        Log.e("RegisterActivity", "Failed to create Firebase user", task.exception)
+                    }
+                }
+        }
+    }   }
